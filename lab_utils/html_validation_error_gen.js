b4adb2b9617493cb924de841ef6e3dd90d8dd413
alexeyjeffers <70725010+alexeyjeffers@users.noreply.github.com>
2020-09-25 16:56:37 -0400

    Update html_validation_error_gen.js

--- lab_utils/html_validation_error_gen.js
+++ lab_utils/html_validation_error_gen.js
@@ -13,7 +13,12 @@ import {
   processHTML,
   excludeNonHTML,
   excludeAnswerKey,
+<<<<<<< HEAD
   severeErrorCheck
+=======
+  severeErrorCheck,
+  excludeInstructions
+>>>>>>> cc14903d35ae2881b3a6a117d700ef58894cc592
   // generateFixtureForTests
 } from './functions.js';
 
@@ -48,15 +53,28 @@ if (obj) {
       console.log('Fixtures updated for testing');
     }
   });
+<<<<<<< HEAD
   recursiveReaddir('./public', [excludeNonHTML, excludeAnswerKey, '.DS_Store'])
+=======
+  recursiveReaddir('./public', [excludeNonHTML, excludeAnswerKey, excludeInstructions, '.DS_Store'])
+>>>>>>> cc14903d35ae2881b3a6a117d700ef58894cc592
   // for each file in data, we're going to want to run the validator and output an updated file.
   // this needs to happen asynchronously because each process is its own thing
     .then((data) => Promise.all(data.map((m) => processHTML(m))))
     .then((data) => data.filter((f) => f.errors.length > 0))
+<<<<<<< HEAD
     // .then((data) => data.map((m) => {
     //   m.title = m.filename.match(/lab_\d+/g)[0]; // TODO: Replace this with a config file
     //   return m;
     // }))
+=======
+    .then((data) => data.map((m) => {
+      const titleArr = m.filename.match(/lab_\d+/g);
+      m.title = titleArr ? titleArr[0] : m.filename;
+      console.log('Checked ', m.title);
+      return m;
+    }))
+>>>>>>> cc14903d35ae2881b3a6a117d700ef58894cc592
     .then((data) => data.map((m) => severeErrorCheck(m)))
     .then((data) => {
       console.log(
