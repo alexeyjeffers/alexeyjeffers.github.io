5c545cbc8fc76610c27756f1ed457f88391f5c4a
alexeyjeffers <70725010+alexeyjeffers@users.noreply.github.com>
2020-10-30 12:59:21 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -14,7 +14,7 @@ function convertRestaurantsToCategories(restaurantList) {
   function getRandomIntInclusive(min, max) {
     const min1 = Math.ceil(min);
     const max1 = Math.floor(max);
-    return Math.floor(Math.random() * (max1 - min1 + 1) + min1); // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max1 - min1 + 1) + min1); 
   }
 
   async function loadData() {
